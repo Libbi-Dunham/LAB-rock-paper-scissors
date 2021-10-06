c3f3493b43a8815856b778562756c7da2e816afe
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-06 13:47:41 -0700

    added my pictures and started some javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('play');
+const winSpan = document.getElementById('play');
+const lossSpan = document.getElementById('play');
+const error = document.getElementById('error');
+
+let win = 0;
+let lost = 0;
+
